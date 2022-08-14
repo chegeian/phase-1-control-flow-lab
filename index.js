9b6026ee1a6acde2c91bc9ef68ee56ff6c0b2544
Chege <chegeian@email.com>
2022-08-14 18:38:11 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Distance){
   // Write your code here!
+  if (Distance <= 400) {
+      return "This one is on me!";
+  }
+  
+  if (Distance >2500) {
+      return "No can do.";
+  } else if (Distance >= 2000) {
+      return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cty){
   // Write your code here!
+  return cty === "NYC" ? "Ok, sounds good.": "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+   // Write your code here!
+  switch(tip) {
+      case 'generous' :
+          return 'Thank you so much.';
+          break;
+      case 'not as generous' :
+          return 'Thank you.';
+          break;
+      default:
+          return 'Bye.';
+          break;
+  }
 }
